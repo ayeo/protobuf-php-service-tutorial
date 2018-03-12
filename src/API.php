bcad47ac75ac01f0a36c96bfc3d2668c2387f2d6
ayeo <me@ayeo.pl>
2018-03-12 17:20:46 +0100

    Use spaces instead of tabs

--- src/API.php
+++ src/API.php
@@ -8,33 +8,33 @@ use Google\Protobuf\Internal\Message;
 
 class API implements CalculatorInterface
 {
-	private $handler;
+    private $handler;
 
-	public function __construct()
-	{
-		$this->handler = new Handler();
-	}
+    public function __construct()
+    {
+        $this->handler = new Handler();
+    }
 
     public function add(AddRequest $request): AddReply
     {
-    	$reply = new AddReply();
-	    $data = $this->makeRequest($request, 'add');
-	    $reply->mergeFromString($data);
+        $reply = new AddReply();
+        $data = $this->makeRequest($request, 'add');
+        $reply->mergeFromString($data);
 
-    	return $reply;
+        return $reply;
     }
 
     public function subtract(SubtractRequest $request): SubtractReply
     {
-    	$reply = new SubtractReply();
-	    $data = $this->makeRequest($request, 'subtract');
-	    $reply->mergeFromString($data);
+        $reply = new SubtractReply();
+        $data = $this->makeRequest($request, 'subtract');
+        $reply->mergeFromString($data);
 
-    	return $reply;
+        return $reply;
     }
 
     private function makeRequest(Message $message, string $method): string
     {
-    	return  $this->handler->run($method, $message->serializeToString());
+        return $this->handler->run($method, $message->serializeToString());
     }
 }
