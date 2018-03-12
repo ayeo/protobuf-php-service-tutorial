c9e4c08275c5eb51d1a387cad2e4da5b60133d09
ayeo <me@ayeo.pl>
2018-03-12 17:21:32 +0100

    Use spaces instead of tabs

--- src/Calculator.php
+++ src/Calculator.php
@@ -8,15 +8,15 @@ class Calculator implements CalculatorInterface
 {
     public function add(AddRequest $request): AddReply
     {
-    	$sum = $request->getX() + $request->getY();
+        $sum = $request->getX() + $request->getY();
 
-    	return (new AddReply())->setSum($sum);
+        return (new AddReply())->setSum($sum);
     }
 
     public function subtract(SubtractRequest $request): SubtractReply
     {
-    	$diff = $request->getX() - $request->getY();
+        $diff = $request->getX() - $request->getY();
 
-    	return (new SubtractReply())->setDiff($diff);
+        return (new SubtractReply())->setDiff($diff);
     }
 }
--- src/Handler.php
+++ src/Handler.php
@@ -6,21 +6,22 @@ namespace Yuloh\Math;
 
 class Handler
 {
-	public function run(string $method, string $requestData): string
-	{
-		switch ($method) {
-			case 'add':
-				$request = new AddRequest();
-				$request->mergeFromString($requestData);
-				$reply = (new Calculator())->add($request);
-				return $reply->serializeToString();
-			case 'subtract':
-				$request = new SubtractRequest();
-				$request->mergeFromString($requestData);
-				$reply = (new Calculator())->subtract($request);
-				return $reply->serializeToString();
-			default:
-				throw new \LogicException('Invalid method');
-		}
-	}
+    public function run(string $method, string $requestData): string
+    {
+        switch ($method)
+        {
+            case 'add':
+                $request = new AddRequest();
+                $request->mergeFromString($requestData);
+                $reply = (new Calculator())->add($request);
+                return $reply->serializeToString();
+            case 'subtract':
+                $request = new SubtractRequest();
+                $request->mergeFromString($requestData);
+                $reply = (new Calculator())->subtract($request);
+                return $reply->serializeToString();
+            default:
+                throw new \LogicException('Invalid method');
+        }
+    }
 }
